3fee5111979f8a0d26f9590a1a84853259bd9e01
SuperDev <marcus.ross@zahlenhelfer.de>
2020-06-24 14:27:53 +0200

    first try to capture images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Test Test';
+export class AppComponent implements OnInit {
+  @ViewChild('video')
+  public video: ElementRef;
+
+  @ViewChild('canvas')
+  public canvas: ElementRef;
+
+  public captures: Array<any>;
+
+  public constructor() {
+    this.captures = [];
+  }
+
+  public ngOnInit() {}
+
+  public ngAfterViewInit() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        this.video.nativeElement.src = window.URL.createObjectURL(stream);
+        this.video.nativeElement.play();
+      });
+    }
+  }
+
+  public capture() {
+    var context = this.canvas.nativeElement
+      .getContext('2d')
+      .drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
+  }
 }
